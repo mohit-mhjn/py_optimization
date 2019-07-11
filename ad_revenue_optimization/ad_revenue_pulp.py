4739df6dbdbef32b2116d028ee8ed70f28ea7d07
mohit_mhjn <mohitmhjn147@gmail.com>
2019-07-11 23:24:52 +0530

    started with pulp model

---
+++ ad_revenue_optimization/ad_revenue_pulp.py
@@ -0,0 +1,9 @@
+# given data
+list_of_campaigns = ["TV","SEO","Adwords","Facebook"]
+roi_values = {"TV":9,"SEO":14,"Adwords":10,"Facebook":5}
+penetration_values = {"TV":2.5,"SEO":2.1,"Adwords":0.9,"Facebook":3}
+budget = 1000000
+viewer_count = 1300000
+
+import pulp 
+
